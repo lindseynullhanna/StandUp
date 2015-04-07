5a8f185694c59924c4824e854a756c11a3eec31b
Lindsey Hanna <lindsey.hanna@workiva.com>
2015-04-06 19:28:16 -0600

    Initial Commit

---
+++ StandUp2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  StandUp2
+//
+//  Created by Lindsey.Hanna on 4/6/15.
+//  Copyright (c) 2015 Lindsey.Hanna. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StandUp2Tests/StandUp2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  StandUp2Tests.swift
+//  StandUp2Tests
+//
+//  Created by Lindsey.Hanna on 4/6/15.
+//  Copyright (c) 2015 Lindsey.Hanna. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class StandUp2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
